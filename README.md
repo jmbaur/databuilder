f1b6771a5c3c1b90f8a5bc6f466f2053404e7167
Jared Baur <jaredbaur@fastmail.com>
2020-11-01 17:45:48 -0700

    Started mocking process. Added a mocking config that can be manipulated using CLI flags

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 ## Usage
 
 `echo "CREATE TABLE temp_table(id SERIAL PRIMARY KEY, name VARCHAR(60));" | databuilder --connection=postgres://localhost:5432`
+
+## Running Postgres
+
+`docker run -e POSTGRES_USER user -e POSTGRES_PASSWORD=pwd -p 5432:5432 -d --rm postgres`
--- cmd/databuilder.go
+++ cmd/databuilder.go
@@ -1,12 +1,35 @@
 package cmd
 
 import (
+	"context"
+	"flag"
+	"log"
 	"os"
+	"strings"
 
+	"github.com/jmbaur/databuilder/db"
 	"github.com/jmbaur/databuilder/mocker"
 )
 
 func Execute() {
 	m := mocker.New("tmp")
 	m.Parse(os.Stdin)
+
+	connString := flag.String("connection", "postgres://localhost:5432", "connection string to the database")
+	ignoreTables := flag.String("ignoreTables", "", "tables to skip when creating mock data")
+	flag.Parse()
+
+	conn, err := db.GetConnection(*connString)
+	if err != nil {
+		log.Fatalf("unable to connect to database: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	mockConfig := &mocker.Config{
+		IgnoreTables: strings.Split(*ignoreTables, ","),
+	}
+
+	if err := m.Mock(conn, mockConfig); err != nil {
+		log.Fatalf("unable to mock the database: %v", err)
+	}
 }
