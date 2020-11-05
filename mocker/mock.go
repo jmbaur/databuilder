5b5441788ae1afe553f41796a8293c9baaaf3ef4
Jared Baur <jaredbaur@fastmail.com>
2020-11-04 23:52:58 -0700

    start inserting mock data into the database

--- mocker/mock.go
+++ mocker/mock.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"regexp"
+	"strconv"
 	"time"
 
 	"github.com/brianvoe/gofakeit/v5"
@@ -41,8 +42,6 @@ func (c *Config) tableSkip(tableName string) bool {
 	return skip
 }
 
-type widget map[string]interface{}
-
 func (m *Mocker) Mock(conn *pgx.Conn, config *Config) error {
 	if err := config.prep(); err != nil {
 		return err
@@ -51,77 +50,86 @@ func (m *Mocker) Mock(conn *pgx.Conn, config *Config) error {
 	gofakeit.Seed(time.Now().UnixNano())
 
 	for _, table := range m.Tables {
-
 		if config.tableSkip(*table.Relation.Relname) {
 			continue
 		}
 
-		var widgets []widget
+		var columns []string
 		for i := 0; i < config.Amount; i++ {
-			w := make(widget)
+			var w []interface{}
 			for _, tableElement := range table.TableElts.Items {
 				column, okColumn := tableElement.(nodes.ColumnDef)
-				_, okConstraint := tableElement.(nodes.Constraint)
+				if !okColumn {
+					continue
+				}
 
-				if okColumn {
-					if column.TypeName == nil {
+				if column.TypeName == nil {
+					continue
+				}
+
+				columnName := *column.Colname
+				columnType := column.TypeName.Names.Items[0].(nodes.String).Str
+
+				var columnValue interface{}
+
+				switch columnType {
+				case "text":
+					columnValue = generateText(columnName, column.IsNotNull)
+				case "date":
+					columnValue = gofakeit.Date().Format(time.RFC3339)
+				case "timestamp":
+					columnValue = gofakeit.Date()
+				case "daterange":
+					date1 := gofakeit.Date()
+					date2 := date1.Add(time.Duration(gofakeit.Number(1, 10000)) * time.Hour)
+					columnValue = "[" + date1.Format(time.RFC3339) + "," + date2.Format(time.RFC3339) + "]"
+				case "pg_catalog":
+					// foreign table reference
+					constraints := column.Constraints.Items
+					constrIndex := findForeignConstraint(constraints)
+					if constrIndex < 0 {
 						continue
 					}
+					foreigncolumn := constraints[constrIndex].(nodes.Constraint).PkAttrs.Items[0].(nodes.String).Str
+					foreigntable := *constraints[constrIndex].(nodes.Constraint).Pktable.Relname
 
-					columnName := *column.Colname
-					columnType := column.TypeName.Names.Items[0].(nodes.String).Str
-
-					var columnValue interface{}
-
-					switch columnType {
-					case "text":
-						columnValue = generateText(columnName, column.IsNotNull)
-					case "date":
-						columnValue = gofakeit.Date()
-					case "timestamp":
-						columnValue = gofakeit.Date()
-					case "daterange":
-						date1 := gofakeit.Date()
-						date2 := date1.Add(time.Duration(gofakeit.Number(1, 10000)) * time.Hour)
-						columnValue = "[" + date1.Format(time.RFC3339) + "," + date2.Format(time.RFC3339) + "]"
-					case "pg_catalog":
-						// foreign table reference
-						constraints := column.Constraints.Items
-						constrIndex := findForeignConstraint(constraints)
-						if constrIndex < 0 {
-							continue
-						}
-						foreigncolumn := constraints[constrIndex].(nodes.Constraint).PkAttrs.Items[0].(nodes.String).Str
-						foreigntable := *constraints[constrIndex].(nodes.Constraint).Pktable.Relname
-
-						columnValue = getRandomForeignRefValue(conn, foreigntable, foreigncolumn)
+					columnValue = getRandomForeignRefValue(conn, foreigntable, foreigncolumn)
+					continue
+				case "serial":
+					continue
+				case "json":
+					json, _ := json.Marshal(struct {
+						Status string `json:"status"`
+					}{Status: "JSON is not yet implemented."})
+					columnValue = json
+				case "bytea":
+					continue
+				default:
+					// is most likely an enum type
+					enumIndex := findEnumDef(m.Enums, columnType)
+					if enumIndex < 0 {
+						log.Printf("could not find enum %s\n", columnType)
 						continue
-					case "serial":
-						continue
-					case "json":
-						json, _ := json.Marshal(struct {
-							Status string `json:"status"`
-						}{Status: "JSON is not yet implemented."})
-						columnValue = json
-					case "bytea":
-						continue
-					default:
-						// is most likely an enum type
-						enumIndex := findEnumDef(m.Enums, columnType)
-						if enumIndex < 0 {
-							log.Printf("could not find enum %s\n", columnType)
-							continue
-						}
-						columnValue = getRandomEnumValue(m.Enums, enumIndex)
 					}
-					w[columnName] = columnValue
+					columnValue = getRandomEnumValue(m.Enums, enumIndex)
 				}
-				if okConstraint {
-					// fmt.Println(constraint)
+				if i == 0 {
+					columns = append(columns, columnName)
 				}
+				w = append(w, columnValue)
 			}
-			widgets = append(widgets, w)
-
+			// insert into  table
+			insert, err := buildInsertStmt(columns, *table.Relation.Relname)
+			if err != nil {
+				log.Printf("failed to build insert statement for table \"%s\": %v", *table.Relation.Relname, err)
+				continue
+			}
+			// fmt.Println(*insert, w)
+			_, err = conn.Query(context.Background(), *insert, w...)
+			if err != nil {
+				// TODO keep trying to build table in this case?
+				log.Println(err)
+			}
 		}
 	}
 	return nil
@@ -165,4 +173,33 @@ func getRandomForeignRefValue(conn *pgx.Conn, foreigntable, foreigncolumn string
 		log.Println(err)
 	}
 	return val
+}
+
+func passesConstraints(widget interface{}, constraints []nodes.Constraint) bool {
+	return true
+}
+
+func buildInsertStmt(columns []string, table string) (*string, error) {
+	if len(columns) == 0 {
+		return nil, fmt.Errorf("table has no columns")
+	}
+	var insert string
+	insert += "INSERT INTO "
+	insert += table + " ("
+	for i, col := range columns {
+		if i == len(columns)-1 {
+			insert += col + ") "
+		} else {
+			insert += col + " "
+		}
+	}
+	insert += "VALUES ("
+	for i := range columns {
+		if i == len(columns)-1 {
+			insert += "$" + strconv.Itoa(i+1) + ")"
+		} else {
+			insert += "$" + strconv.Itoa(i+1) + ", "
+		}
+	}
+	return &insert, nil
 }
