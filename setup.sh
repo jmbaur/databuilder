3fda96239eb21d8d17f9201aba27ba4c55d09d91
Jared Baur <jaredbaur@fastmail.com>
2020-11-29 19:39:40 -0700

    shellcheck setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ set -e
 
 podman run -d --rm --name databuilder -e POSTGRES_USER=user -e POSTGRES_PASSWORD=pwd -e POSTGRES_DB=test -p 6543:5432 postgres
 sleep 5
-cat ./assets/test_large.sql | psql postgres://user:pwd@localhost:6543/test
+psql postgres://user:pwd@localhost:6543/test < ./assets/test_large.sql
